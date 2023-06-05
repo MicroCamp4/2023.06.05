c612e9b47e29fa46fdb42345cb66d03d3aa55543
Mateusz <mat.bereda@gmail.com>
2023-06-05 12:38:00 +0200

    nowa linia

--- src/main/java/pl/comarch/szkolenia/git/App.java
+++ src/main/java/pl/comarch/szkolenia/git/App.java
@@ -3,5 +3,7 @@ package pl.comarch.szkolenia.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("GIT");
+
+        System.out.println("nowa linijka !!!");
     }
 }
